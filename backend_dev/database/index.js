aa8162822ebd2eb4015560130b6498c3c33f45da
ThiriNyeinAye <thirinyeinaye23@gmail.com>
2023-12-18 14:05:42 +0630

    config routes

--- backend_dev/database/index.js
+++ backend_dev/database/index.js
@@ -5,27 +5,26 @@ require("dotenv").config();
 
 console.log("user >> ", process.env.DB_NAME, process.env.DB_USER);
 
-const DBConnect = new Sequelize("energy_report", "root", "root1234", {
-  host: "127.0.0.1",
-  dialect: "mysql",
-  dialectOptions: {
-    multipleStatements: true,
-  },
-});
+const DBConnect = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    dialectOptions: {
+      multipleStatements: true,
+    },
+  }
+);
 
-// DBConnect.authenticate()
-//   .then(() => {
-//     console.log("Connection is successful.");
-//     // EnergyConsumptionModel.sync().then(() => {
-//     //   console.log("table is created");
-//     // });
-//     // AbnormalitiesModel.sync().then(() => {
-//     //   console.log("table is created");
-//     // });
-//   })
-//   .catch((error) => {
-//     console.error("Connection is failed.", error);
-//   });
+DBConnect.authenticate()
+  .then(() => {
+    console.log("Connection is successful.");
+  })
+  .catch((error) => {
+    console.error("Connection is failed.", error);
+  });
 
 module.exports = {
   DBConnect,
--- backend_dev/routes/index.js
+++ backend_dev/routes/index.js
@@ -1,5 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const {
+  abnormalitiesController,
+} = require("../controller/abnormalitiesController");
+const { waterUsageController } = require("../controller/waterUasgeController");
+const {
+  electricityController,
+} = require("../controller/electricityController");
+const {
+  carbonFootprintController,
+} = require("../controller/carbonFootprintController");
+const {
+  energyConsumptionController,
+} = require("../controller/energyConsumptionController");
+const { waterLevelController } = require("../controller/waterLevelController");
+const { recycleController } = require("../controller/recycleController");
 
-router.get("/energy-consumption");
+router.get("/abnormalities", abnormalitiesController);
+router.get("/water-usage", waterUsageController);
+router.get("/water-level", waterLevelController);
+router.get("/electricity-usage", electricityController);
+router.get("/carbon-footprint", carbonFootprintController);
+router.get("/energy-consumption", energyConsumptionController);
+router.get("/recycle-data", recycleController);
+
 module.exports = router;
